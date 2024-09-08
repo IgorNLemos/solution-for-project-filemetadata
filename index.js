aa1e979a441e564c1bd809756d3cfbf34bdac6db
IgorNLemos <147350759+IgorNLemos@users.noreply.github.com>
2024-09-08 20:11:41 +0000

    Completed solution for fcc file metadata microservice

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
+const path = require('path');
+
+
 require('dotenv').config()
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -12,6 +16,33 @@ app.get('/', function (req, res) {
 });
 
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
+  }
+});
+
+const upload = multer({dest:'uploads/'});
+
+// Serve static files from the 'uploads' directory
+//app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  
+   if (!req.file) {
+     res.json("No file uploaded. Please upload a file.")
+   } else {
+   res.json({
+     name: req.file.originalname,
+     type: req.file.mimetype,
+     size: req.file.size,
+   });
+  }
+})
 
 
 const port = process.env.PORT || 3000;
